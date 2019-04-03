46d79e73bb21d54c2d61ee9f9cf522f95148d87b
Musgrove, Forrest E <forrest-musgrove@uiowa.edu>
2019-04-03 00:35:10 -0500

    Added a CMakeLists file as well as a main file that prints out the command line arguments passed

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// EcoSim - Ecosystem Simulator CLI
+// Author - Forrest Musgrove
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    cout << "You have entered " << argc << " arguments:" << "\n";
+
+    for (int i = 0; i < argc; ++i)
+        cout << argv[i] << "\n";
+
+    return 0;
+}
