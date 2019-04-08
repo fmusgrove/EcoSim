cfd1edcf1ba23ef7f78f580b9aecc4c2ac08944d
Musgrove, Forrest E <forrest-musgrove@uiowa.edu>
2019-04-08 16:17:25 -0500

    Added a function to load the terrain map in color

--- main.cpp
+++ main.cpp
@@ -13,13 +13,19 @@
 
 using namespace std;
 
+void drawMap(unique_ptr<vector<vector<char>>> &map);
+
 int main(int argc, char **argv) {
+    string script_filepath(argv[0]);
     string mapFilePath, speciesFilePath, fileLine;
-    vector<vector<char>> map;
+    auto map = make_unique<vector<vector<char>>>();
     // Get arguments and set default map and species files if none are specified
     if (argc >= 3) {
         mapFilePath = argv[1];
         speciesFilePath = argv[2];
+    } else if (script_filepath.find("cmake") != string::npos) {
+        mapFilePath = "../default_input/map.txt";
+        speciesFilePath = "../default_input/species.txt";
     } else {
         mapFilePath = "default_input/map.txt";
         speciesFilePath = "default_input/species.txt";
@@ -29,7 +35,8 @@ int main(int argc, char **argv) {
     ifstream mapFile(mapFilePath);
     if (mapFile.is_open()) {
         while (getline(mapFile, fileLine)) {
-            cout << fileLine << '\n';
+            std::vector<char> line_data(fileLine.begin(), fileLine.end());
+            map->push_back(line_data);
         }
         mapFile.close();
     } else {
@@ -39,9 +46,19 @@ int main(int argc, char **argv) {
 
     // Initialize curses and setup main screen
     initscr();
+    if (has_colors() == FALSE) {
+        printf("Your terminal does not support color\n");
+    } else {
+        start_color();
 
+        // Define color pairs for varying map elements
+        init_pair(0, COLOR_WHITE, COLOR_BLACK);
+        init_pair(1, COLOR_GREEN, COLOR_BLACK);
+        init_pair(2, COLOR_BLUE, COLOR_BLACK);
+        init_pair(3, COLOR_RED, COLOR_BLACK);
+    }
 
-    refresh();
+    drawMap(map);
 
     // Wait for user input
     getch();
@@ -49,4 +66,35 @@ int main(int argc, char **argv) {
     // End curses mode and exit
     endwin();
     return 0;
+}
+
+void drawMap(unique_ptr<vector<vector<char>>> &map) {
+    for (auto &mapLineVector: *map) {
+        for (char &lineChar: mapLineVector) {
+            switch (lineChar) {
+                case '~' :
+                    // Water
+                    attron(COLOR_PAIR(2));
+                    addch(lineChar);
+                    attroff(COLOR_PAIR(2));
+                    break;
+                case '#':
+                    // Obstacle
+                    attron(COLOR_PAIR(3));
+                    addch(lineChar);
+                    attroff(COLOR_PAIR(3));
+                    break;
+                default:
+                    // Default to green on black text
+                    attron(COLOR_PAIR(1));
+                    addch(lineChar);
+                    attroff(COLOR_PAIR(1));
+                    break;
+            }
+
+        }
+        addch('\n');
+    }
+
+    refresh();
 }
