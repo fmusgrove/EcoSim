01ee5e2f5e33a6358b0010a438706f6fe841d778
Musgrove, Forrest E <forrest-musgrove@uiowa.edu>
2019-04-13 18:30:06 -0500

    Added logic to parse the species file and load the data into a map

--- main.cpp
+++ main.cpp
@@ -6,8 +6,11 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <map>
+#include <unordered_map>
+#include <algorithm>
 #include "ncurses.h"
 
 #include "species_type.hpp"
@@ -22,11 +25,20 @@ void drawMap(const unique_ptr<WaterObstacleList> &waterObstacle, const unique_pt
 // TODO: Make coordinates relative to the map starting point so curses can draw it wherever, such as the
 //  center of the screen
 
+struct SpeciesTraits {
+public:
+    string speciesType;
+    int regrowthCoeff;
+    int energy;
+    vector<char> foodChain;
+};
+
 int main(int argc, char **argv) {
     string script_filepath(argv[0]);
     string mapFilePath, speciesFilePath, fileLine;
     auto floraFauna = make_unique<FloraFaunaList>();
     auto terrain = make_unique<WaterObstacleList>();
+
     // Get arguments and set default map and species files if none are specified
     if (argc >= 3) {
         mapFilePath = argv[1];
@@ -37,6 +49,64 @@ int main(int argc, char **argv) {
     } else {
         mapFilePath = "default_input/map.txt";
         speciesFilePath = "default_input/species.txt";
+    }
+
+    // Load species list
+    ifstream speciesFile(speciesFilePath);
+    istringstream stringTraitStream;
+    string traitString;
+    unordered_map<char, SpeciesTraits> speciesList;
+    if (speciesFile.is_open()) {
+        while (getline(speciesFile, fileLine)) {
+            stringTraitStream.clear();
+            stringTraitStream.str(fileLine);
+            string speciesType;
+            char speciesID;
+            vector<char> foodChain;
+            int regrowthCoeff = -1;
+            int energy = -1;
+
+            // Read the species type
+            stringTraitStream >> traitString;
+            speciesType = traitString;
+            // Read the character ID
+            stringTraitStream >> traitString;
+            speciesID = traitString[0];
+
+            // Read the rest of the traits
+            do {
+                stringTraitStream >> traitString;
+
+                // Assemble food chain
+                if (traitString.find('[') != string::npos || traitString.find(']') != string::npos ||
+                    traitString.find(',') != string::npos) {
+                    traitString.erase(std::remove(traitString.begin(), traitString.end(), '['), traitString.end());
+                    traitString.erase(std::remove(traitString.begin(), traitString.end(), ']'), traitString.end());
+                    traitString.erase(std::remove(traitString.begin(), traitString.end(), ','), traitString.end());
+                    foodChain.push_back(traitString[0]);
+                    continue;
+                }
+
+                // Set regrowth coefficient for plants and energy levels for both plants and animals
+                if (speciesType == "plant") {
+                    if (regrowthCoeff == -1) {
+                        regrowthCoeff = stoi(traitString);
+                    } else if (energy == -1) {
+                        energy = stoi(traitString);
+                    }
+                } else {
+                    energy = stoi(traitString);
+                }
+
+            } while (stringTraitStream);
+
+            // Add species definition to the reference map
+            speciesList.insert(pair<char, SpeciesTraits>(speciesID, {speciesType, regrowthCoeff, energy, foodChain}));
+        }
+        speciesFile.close();
+    } else {
+        cerr << "Unable to open file '" << speciesFilePath << "'" << endl;
+        return -1;
     }
 
     // Load map into memory
