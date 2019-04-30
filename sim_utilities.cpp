80395c182de048081f58774ff05a13084518aab3
Musgrove, Forrest E <forrest-musgrove@uiowa.edu>
2019-04-30 00:10:46 -0500

    Added header for simulation utilities and added functions for prompting the user for values to the same

--- sim_utilities.cpp
+++ sim_utilities.cpp
@@ -1,71 +1,142 @@
-#ifndef ECOSIM_SIM_UTILITIES_CPP
-#define ECOSIM_SIM_UTILITIES_CPP
+#include "sim_utilities.hpp"
 
-#include <string>
-#include "ncurses.h"
-#include "ecosystem_element.hpp"
-#include "map_manager.hpp"
-
-using namespace std;
-
-struct SpeciesTraits {
-public:
-    string speciesType;
-    int regrowthCoeff;
-    int energy;
-    vector<char> foodChain;
-};
-
-void drawMap(WINDOW *window, const int mapOffsetY, const int mapOffsetX) {
-    // Cursor location entered in the form row, column (y, x)
-    // Draw terrain
-    for (auto &pointCharPair: MapManager::terrain) {
-        switch (pointCharPair.second) {
-            case '~' :
-                // Water
-                wattron(window, COLOR_PAIR(2));
-                mvwaddch(window, pointCharPair.first.second + mapOffsetY, pointCharPair.first.first + mapOffsetX, '~');
-                wattroff(window, COLOR_PAIR(2));
-                break;
-            case '#':
-                // Obstacle
-                wattron(window, COLOR_PAIR(3));
-                mvwaddch(window, pointCharPair.first.second + mapOffsetY, pointCharPair.first.first + mapOffsetX, '#');
-                wattroff(window, COLOR_PAIR(3));
-                break;
-            default:
-                break;
+namespace SimUtilities {
+    void drawMap(WINDOW *window, const int mapOffsetY, const int mapOffsetX, bool has_border) {
+        // Clear the window
+        wclear(window);
+        // Redraw the border if present
+        if (has_border) {
+            wattron(window, COLOR_PAIR(1));
+            box(window, 0, 0);
+            wattroff(window, COLOR_PAIR(1));
         }
+
+        // Cursor location entered in the form row, column (y, x)
+        // Draw terrain
+        for (auto &pointCharPair: MapManager::terrain) {
+            switch (pointCharPair.second) {
+                case '~' :
+                    // Water
+                    wattron(window, COLOR_PAIR(2));
+                    mvwaddch(window, pointCharPair.first.second + mapOffsetY, pointCharPair.first.first + mapOffsetX,
+                             '~');
+                    wattroff(window, COLOR_PAIR(2));
+                    break;
+                case '#':
+                    // Obstacle
+                    wattron(window, COLOR_PAIR(3));
+                    mvwaddch(window, pointCharPair.first.second + mapOffsetY, pointCharPair.first.first + mapOffsetX,
+                             '#');
+                    wattroff(window, COLOR_PAIR(3));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Draw plants and animals
+        for (auto &element: MapManager::floraFauna) {
+            wattron(window, COLOR_PAIR(element.second->getColorPair()));
+            mvwaddch(window, element.first.second + mapOffsetY, element.first.first + mapOffsetX,
+                     element.second->getCharID());
+            wattroff(window, COLOR_PAIR(element.second->getColorPair()));
+        }
+
+        wmove(window, 1, 1);
+        wrefresh(window);
     }
 
-    // Draw plants and animals
-    for (auto &element: MapManager::floraFauna) {
-        wattron(window, COLOR_PAIR(element.second->getColorPair()));
-        mvwaddch(window, element.first.second + mapOffsetY, element.first.first + mapOffsetX,
-                 element.second->getCharID());
-        wattroff(window, COLOR_PAIR(element.second->getColorPair()));
+    WINDOW *createWindow(int height, int width, int startY, int startX, bool addBorders) {
+        auto local_win = newwin(height, width, startY, startX);
+        // Add border around the window
+        if (addBorders) {
+            wattron(local_win, COLOR_PAIR(1));
+            box(local_win, 0, 0);
+            wattroff(local_win, COLOR_PAIR(1));
+        }
+        // Show the border
+        wrefresh(local_win);
+        return local_win;
     }
 
-    wrefresh(window);
-}
-
-WINDOW *createWindow(int height, int width, int startY, int startX, bool addBorders) {
-    auto local_win = newwin(height, width, startY, startX);
-    // Add border around the window
-    if (addBorders) {
-        box(local_win, 0, 0);
+    void destroyWindow(WINDOW *local_win) {
+        // Erase the window borders
+        wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+        wrefresh(local_win);
+        // Remove the window
+        delwin(local_win);
     }
-    // Show the border
-    wrefresh(local_win);
-    return local_win;
-}
 
-void destroyWindow(WINDOW *local_win) {
-    // Erase the window borders
-    wborder(local_win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-    wrefresh(local_win);
-    // Remove the window
-    delwin(local_win);
-}
+    void printStringToWindow(WINDOW *window, const char *printString, bool has_border) {
+        int x, y;
+        getyx(window, y, x);
 
-#endif //ECOSIM_SIM_UTILITIES_CPP
+        if (y == 0) {
+            ++y;
+        }
+
+        int maxY = getmaxy(window);
+
+        if (y >= maxY - 1) {
+            wclear(window);
+            wmove(window, 1, 1);
+            getyx(window, y, x);
+
+            if (has_border) {
+                wattron(window, COLOR_PAIR(1));
+                box(window, 0, 0);
+                wattroff(window, COLOR_PAIR(1));
+            }
+        }
+
+        mvwprintw(window, y, 1, "%s", printString);
+        wrefresh(window);
+    }
+
+    int windowPromptInt(WINDOW *window, const char *promptString, int bufferSize) {
+        int userInputNum = 0;
+        bool shouldRetry;
+        char input_str[bufferSize];
+        do {
+            printStringToWindow(window, promptString, true);
+            wgetstr(window, input_str);
+            try {
+                userInputNum = stoi(input_str);
+                shouldRetry = false;
+            } catch (invalid_argument &invArg) {
+                shouldRetry = true;
+                printStringToWindow(window, "Please enter a valid numeric value\n", true);
+            }
+        } while (shouldRetry);
+
+        return userInputNum;
+    }
+
+    string windowPromptStr(WINDOW *window, const char *promptString, vector<string> &allowedValues, int bufferSize) {
+        string userInputStr;
+        bool shouldRetry;
+        char input_str[bufferSize];
+        do {
+            printStringToWindow(window, promptString, true);
+            wgetstr(window, input_str);
+            userInputStr = input_str;
+            transform(userInputStr.begin(), userInputStr.end(), userInputStr.begin(), ::tolower);
+            if (userInputStr.empty() ||
+                find(allowedValues.begin(), allowedValues.end(), userInputStr) == allowedValues.end()) {
+                shouldRetry = true;
+                printStringToWindow(window, "Please enter a valid option\n", true);
+            } else {
+                shouldRetry = false;
+            }
+        } while (shouldRetry);
+
+        return userInputStr;
+    }
+
+    std::vector<Point> random_select(const std::vector<Point> &locations, size_t count) {
+        vector<Point> selection;
+        std::sample(locations.begin(), locations.end(), std::back_inserter(selection),
+                    count, std::mt19937{std::random_device{}()});
+        return selection;
+    }
+}
---
+++ sim_utilities.hpp
@@ -0,0 +1,36 @@
+#ifndef ECOSIM_SIM_UTILITIES_HPP
+#define ECOSIM_SIM_UTILITIES_HPP
+
+#include <string>
+#include <random>
+#include <iterator>
+#include "ncurses.h"
+#include "ecosystem_element.hpp"
+#include "map_manager.hpp"
+
+namespace SimUtilities {
+
+    struct SpeciesTraits {
+    public:
+        string speciesType;
+        int regrowthCoeff;
+        int energy;
+        vector<char> foodChain;
+    };
+
+    void drawMap(WINDOW *window, const int mapOffsetY, const int mapOffsetX, bool has_border);
+
+    WINDOW *createWindow(int height, int width, int startY, int startX, bool addBorders);
+
+    void destroyWindow(WINDOW *local_win);
+
+    void printStringToWindow(WINDOW *window, const char *printString, bool has_border);
+
+    int windowPromptInt(WINDOW *window, const char *promptString, int bufferSize);
+
+    string windowPromptStr(WINDOW *window, const char *promptString, vector<string> &allowedValues, int bufferSize);
+
+    std::vector<Point> random_select(const std::vector<Point> &locations, size_t count);
+}
+
+#endif //ECOSIM_SIM_UTILITIES_HPP
